6f2cae32a0ea2a867e6361e4b8ae39dd9c646e23
FuturePay <lchisholm@futurepay.com>
2017-06-23 20:32:42 +0000

    clear out /var/www before unzipping into it

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="preserve symlinks"
+    release_note="clear out /var/www before unzipping into it"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
