8b457b1eef8d3cece7b34629dfb2d1224581dc9d
FuturePay <lchisholm@futurepay.com>
2017-07-26 22:22:08 +0000

    woopsies, changed  to

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="added in some confd config files"
+    release_note="woopsies, changed  to "
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
