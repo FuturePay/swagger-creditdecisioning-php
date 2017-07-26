c85368969514c3e19c0bee6cf84a791eec1f4199
FuturePay <lchisholm@futurepay.com>
2017-07-26 17:32:50 +0000

    some cleanup of the credit decisioning service

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="removed redundant base-path and renamed service to be creditdecisioning.futurepay.com"
+    release_note="some cleanup of the credit decisioning service"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
