65905b3d8b0b15806056e73dfb2ad474c65a6a01
FuturePay <lchisholm@futurepay.com>
2017-08-02 17:30:52 +0000

    added https scheme for non local environments
    Signed-off-by: Luke Chisholm <lchisholm@futurepay.com>

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="updated the task definition a little bit to have a better log group
+    release_note="added https scheme for non local environments
 
 Signed-off-by: Luke Chisholm <lchisholm@futurepay.com>"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
