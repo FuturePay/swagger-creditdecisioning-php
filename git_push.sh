7a5b2d52763cec64479a6b2ed9583140bc6d4db5
FuturePay <lchisholm@futurepay.com>
2017-09-16 18:45:42 +0000

    FP-2641 Add thin credit file to steps

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="changed stash credentials"
+    release_note="FP-2641 Add thin credit file to steps"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
