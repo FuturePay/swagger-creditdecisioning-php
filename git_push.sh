da91cd0c4ab6f8d31f55f4f192e2ded76eeeda09
FuturePay <lchisholm@futurepay.com>
2017-10-26 16:50:08 +0000

    changed default db ip to correct one

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="FP-2641 Add thin credit file to steps"
+    release_note="changed default db ip to correct one"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
