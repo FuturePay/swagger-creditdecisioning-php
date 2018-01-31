e102d1e03cea9b71e00e800b3ad748f4e7353807
FuturePay <lchisholm@futurepay.com>
2018-01-31 22:30:06 +0000

    FP-2742 Add id mismatch alert type to credit info

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="some pipeline cleanup"
+    release_note="FP-2742 Add id mismatch alert type to credit info"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
